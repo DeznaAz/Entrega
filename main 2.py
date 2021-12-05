48b85dc3f38f48b2d66c89eb1511dfc9f315d7c4
DeznaAz <95508582+DeznaAz@users.noreply.github.com>
2021-12-05 13:04:27 -0600

    Add files via upload
    4. variables

---
+++ main 2.py
@@ -0,0 +1,25 @@
+edad = 24
+altura = 1.5
+institucion = "UNAM"  #nombre-institucion, valor-UNAM
+
+nombre_amigo = "Dezna Aviles Zuniga"
+nombre_amigo_2 = "Luis Roomie"
+
+print(nombre_amigo)
+print(edad)
+print(institucion)
+print("Xavier")
+print("UNAM")
+print(26)
+
+edad = 18
+nombre_amigo = "Esperanza Aviles Zuniga"
+print(nombre_amigo)
+print(edad)
+
+edad = 24  #numero entero
+altura = 1.5  #numero flotante
+suma = 6 + 5
+resta = 6 - 5
+multiplicacion = 6 * 5
+division = 6 / 5
